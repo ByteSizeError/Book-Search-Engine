0c03c625fe129f99a05cdfac59cff01ff38dbc6b
Jimmy Zheng <jimmyz0622@gmail.com>
2022-01-09 13:29:56 -0800

    Add mutations file for frontend

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,59 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook(
+        $author: [String]!
+        $description: String!
+        $title: String!
+        $image: String!
+        $link: String!
+    ) {
+        saveBook(
+            author: $author
+            description: $description
+            title: $title
+            image: $image
+            link: $link
+        ) {
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
